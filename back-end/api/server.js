e60b27e8d6724a1fc5de2b55b293701dcb98af4c
ArturLLopes <arturllopes@gmail.com>
2025-02-14 19:28:22 -0300

    ajuste para deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -1,22 +1,33 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
+
 
 const app = express();
 const PORT = 5005;
 
 app.use(cors());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Olá, MUNDO!");
 });
 
-app.get("/artists", async (req, res) => {
-    res.send(await db.collection("artists").find({}).toArray());
-  });
-  app.get("/songs", async (req, res) => {
-    res.send(await db.collection("songs").find({}).toArray());
-  });
+app.get("/api/artists", async (req, res) => {
+  res.send(await db.collection("artists").find({}).toArray());
+});
+
+app.get("/api/songs", async (req, res) => {
+  res.send(await db.collection("songs").find({}).toArray());
+});
+
+app.use(express.static(path.join(__dirname, "../front-end/dist")));
+
+app.get("*", async (req, res) => {
+  res.sendFile(path.join(__dirname,"../front-end/dist/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
